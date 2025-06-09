52f5c25c5ff5acad5e5e230fcf37d69e0a308340
MERCY NJAMBI <njambim695@gmail.com>
2025-06-09 23:47:56 +0300

    updates

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,62 @@
 import React from 'react'
 import {assets } from '../assets/assets.js'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const [showMenu ,setShowmenu] = useState(false);
+    const [token, setToken] = useState(true); 
+
+    // {have  token when logged in  ''''true when logged in}
+
   return (
     <div  className='flex items-center justify-between text-sm py-4 border-b border-b-gray-400 '>
 <img className='w-44 cursor-pointer'
  src={assets.logo} alt='logo'/>
 <ul className='hidden md:flex items-start gap-5 '>
-    <NavLink> 
+    <NavLink to='/'> 
         <li className='py-1'>HOME</li>  
-        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto'/>
+        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto hidden'/>
     </NavLink>
-    <NavLink> 
+    <NavLink to='/doctors'> 
         <li className='py-1'>ALL DOCTORS</li>  
-        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto'/>
+        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto hidden'/>
     </NavLink>
-    <NavLink> 
+    <NavLink to='/about'> 
         <li className='py-1'>ABOUT</li>  
-        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto'/>
+        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto hidden'/>
     </NavLink>
-    <NavLink> 
+    <NavLink to='/contact'> 
         <li className='py-1'>CONTACT</li>  
-        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto'/>
+        <hr className='border-none outline-none  h-0.5 bg-primary w-3/5 m-auto hidden'/>
     </NavLink>
 </ul>
 
-<div>
-    <button>Create account</button>
+<div className='flex items-center gap-4'>
+    {
+        token?  
+        <div className='flex items-center gap-2 cursor-pointer group relative'>
+            <img className='w-8 rounded-full'
+             src={assets.profile_pic} alt="profile"/>
+            <img className='w-2.5 ' src={assets.dropdown_icon} alt="dropdown"/>
+
+               <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600  z-20 hidden  group-hover:block'>
+                  <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4 '>
+                    <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                    <p onClick={()=>navigate('my-appointments')}  className='hover:text-black cursor-pointer'>My Appointments</p>
+                    <p onClick={()=> setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                  </div>
+               </div>
+            
+            
+        </div>
+        : 
+         <button onClick={()=> navigate('/login')} 
+    className='bg-primary text-white px-8  py-3 rounded-full  font-light hidden md:block'>Create account</button>
+    }
+    
 </div>
     </div>
   )
