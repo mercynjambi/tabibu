a8b3f4f5bf9e06837b86679142b4ce733cf439dc
MERCY NJAMBI <njambim695@gmail.com>
2025-06-23 01:33:57 +0300

    Set up Doctor filtering functionality

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext.jsx'
+
 
 const Doctors = () => {
+
+  const {speciality} = useParams()
+  const [filterDoc, setFilterDoc] = useState([])
+  //  {console.log(speciality)}
+  const {doctors} = useContext(AppContext)
+   const applyFilter = () =>{
+    if(speciality){
+      setFilterDoc(doctors.filter(doc => doc.speciality=== speciality))
+    }else{
+      setFilterDoc(doctors)
+    }
+   }
+   useEffect(()=>{
+    applyFilter()
+   }, [doctors, speciality])
+
+
+
+
   return (
-    <div>Doctors</div>
+    <div>
+    <p>Browse through the doctors specialist</p>
+      <div>
+            <div>
+              <p>General Physician</p>
+              <p>Gynecologist</p>
+              <p>Dermatologist</p>
+              <p>Pediatrcians</p>
+              <p>Neurologist</p>
+              <p>Gastroenterologist</p>
+            </div>
+
+            <div>
+              {
+             filterDoc.map((item,index)=>( 
+                <div onClick={()=> navigate(`/appointment/${item._id}`)} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                    <img className='bg-blue-50' src={item.image} alt={item.name}  />
+                    <div className='p-4'>
+                        <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                              <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                        </div>
+                        <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                         <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                    </div>           
+                    
+                </div>
+            ))
+              }
+            </div>
+      </div>
+    </div>
   )
 }
 
