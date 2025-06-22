bb1f0009ecbc5f3a27356d40abcef88ed6c1dc19
MERCY NJAMBI <njambim695@gmail.com>
2025-06-22 23:05:42 +0300

    updated doctors' functionality

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import { renderToPipeableStream } from 'react-dom/server';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,6 +10,9 @@ export default {
     extend: {
         colors:{
             'primary':"#5f6FFF"
+         },
+         gridTemplateColumns:{
+          'auto': 'repeat(auto-fill, minmax(200px , 1fr))'
          }
     },
   },
